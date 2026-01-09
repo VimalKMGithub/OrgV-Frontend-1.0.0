28b766f16f2875131a28f8cea649d66674251329
Vimal Kumar Mishra <vimalkumarm472@gmail.com>
2026-01-09 13:03:51 +0530

    added proxy in vite dev & preview too so not to only rely on nginx for dev purpose

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,40 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+
   server: {
     port: 9225,
     strictPort: true,
+
+    // =========================
+    // DEV REVERSE PROXY
+    // =========================
+    proxy: {
+      '/api': {
+        target: 'http://localhost:9000',
+        changeOrigin: true,
+        secure: false,
+
+        // STRIP /api
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
   },
+
   preview: {
     port: 9250,
     strictPort: true,
+
+    // =========================
+    // PREVIEW REVERSE PROXY
+    // =========================
+    proxy: {
+      '/api': {
+        target: 'http://localhost:9000',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
   },
 })
